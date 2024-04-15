18d83cb44b0871e9107187602c568e3d1440b069
mzurera- <mzurera-@student.42malaga.com>
2024-04-15 10:30:44 +0200

    Fixxed indentation
    The identation of some files where set to 2 tabs instead of 1.

--- 001/C/multiples-of-3-or-5.c
+++ 001/C/multiples-of-3-or-5.c
@@ -3,27 +3,27 @@
 
 unsigned int sum_multiples_3_or_5(unsigned int n)
 {
-		unsigned int sum;
+	unsigned int sum;
 
-		sum = 0;
-		for (int i = 1; i < n; i++)
-		{
-				if (i % 3 == 0 || i % 5 == 0)
-						sum += i;
-		}
-		return (sum);
+	sum = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	return (sum);
 }
 
 int	main(int argc, char **argv)
 {
-		unsigned int	n = 0;
-		unsigned int	result = 0;
+	unsigned int	n = 0;
+	unsigned int	result = 0;
 
-		if (argc > 1)
-				n = atoi(argv[1]);
-		if (n == 0)
-				n = 1000;
-		result = sum_multiples_3_or_5(n);
-		printf("%d\n", result);
-		return (0);
+	if (argc > 1)
+		n = atoi(argv[1]);
+	if (n == 0)
+		n = 1000;
+	result = sum_multiples_3_or_5(n);
+	printf("%d\n", result);
+	return (0);
 }
